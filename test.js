675e0bc60e4d1799301797f862bec0da353592cc
Paul Vorbach <paul@vorb.de>
2012-09-10 11:25:59 +0200

    travis tests

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-var sha1 = require('./');
+var sha1 = require('./sha1.js');
+var assert = require('assert');
 
-console.log(sha1('message'));
+describe('sha1', function () {
+  it ('should return the expected SHA-1 hash for "message"', function () {
+    assert.equal('6f9b9af3cd6e8b8a73c2cdced37fe9f59226e27d', sha1('message'));
+  });
+});
